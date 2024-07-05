86bfc5ee2d4370846949083dd6dc613d43ae0bf3
Alessandro <79211449+ares-17@users.noreply.github.com>
2024-07-05 20:14:45 +0200

    Update setup-fork.sh
    update setup-fork for windows support

--- setup-fork.sh
+++ setup-fork.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-
 # To retrieve latest version of locators-automated-tests
 git submodule update --init --recursive
-
 # To obtain the tags to be used
 git remote add unina-upstream https://github.com/reverse-unina/A1-ContactList.git
 git remote add upstream https://github.com/ares-17/A1-ContactList.git
